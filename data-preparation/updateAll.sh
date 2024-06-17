0afe92a62d4aea391fbe4f0513d46a58fe183d5c
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-17 16:34:40 +0200

    feat: Add confirmation prompt before executing update scripts
    The commit adds a confirmation prompt before executing the update scripts in the `updateAll.sh` file. This ensures that the user explicitly confirms their intention to continue with the script execution.

--- data-preparation/updateAll.sh
+++ data-preparation/updateAll.sh
@@ -1,6 +1,16 @@
 ROOT_DIR=$(pwd)
 UPDATE_SCRIPTS_DIR=${ROOT_DIR}/update-scripts
 
+# Warning message
+echo "WARNING: Please make sure Blazegraph is switched off before running this script to avoid corruption of the blazegraph journal."
+
+# Confirmation prompt
+read -p "Are you sure you want to continue? (y/n): " answer
+if [[ $answer != "y" ]]; then
+    echo "Script execution aborted."
+    exit 1
+fi
+
 cd ${UPDATE_SCRIPTS_DIR}
 ./updateAat.sh
 ./updateGeonames.sh
