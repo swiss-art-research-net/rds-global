7716c81501c019ec8e2913ad478279f8232b6087
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-17 16:50:57 +0200

    update scripts

--- data-preparation/groupingFeatureAdjustment/start.sh
+++ data-preparation/groupingFeatureAdjustment/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export BLAZEGRAPH_ENDPOINT="http://localhost:8081/blazegraph/sparql"
+if [ -z "${BLAZEGRAPH_ENDPOINT}" ]; then
+    export BLAZEGRAPH_ENDPOINT="http://localhost:8081/blazegraph/sparql"
+fi
 export WIKIDATA_ENDPOINT="https://query.wikidata.org/sparql"
 
 echo "Build Image if not exists"
