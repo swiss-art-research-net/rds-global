4a2c457a54522034921a9f2c49561a2fedc72db2
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-17 13:11:11 +0200

    add type mapping

--- data-preparation/uploadMetadata.sh
+++ data-preparation/uploadMetadata.sh
@@ -17,4 +17,8 @@ echo "Uploading ontology extensions to blazegraph"
 export ONTOLOGY_NAMED_GRAPH=file:%2F%2F%2Frds-ontologies-description.ttl
 curl -D- -L -u guest:guest -H "Content-Type: ${DATA_FORMAT}" --upload-file "./rds-ontologies-description.ttl" -X POST "${BLAZEGRAPH_ENDPOINT}?context-uri=${ONTOLOGY_NAMED_GRAPH}"
 
+echo "Uploading type mappings to blazegraph"
+export TYPE_MAPPING_NAMED_GRAPH=http:%2F%2Fschema.swissartresearch.net%2Frds%2Ftype-mapping
+curl -D- -L -u guest:guest -H "Content-Type: ${DATA_FORMAT}" --upload-file "./manual-preparation/type-mapping.ttl" -X POST "${BLAZEGRAPH_ENDPOINT}?context-uri=${TYPE_MAPPING_NAMED_GRAPH}"
+
 echo "Script uploadMetadata.sh finished."
