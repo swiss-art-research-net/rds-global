2f72665ce7e26324870ac4dd8ad2083d5d20797a
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-19 10:55:14 +0100

    chore: Update download script to include error handling and folder creation
    This commit updates the `_downloadBlazegraphRunner.sh` script in the `data-preparation/update-scripts` directory. It adds the `set -e` flag to ensure the script exits on any error. Additionally, it creates the `../utils` directory if it does not already exist before extracting the blazegraph-runner.
    
    Co-authored-by: [Author Name]

--- data-preparation/update-scripts/_downloadBlazegraphRunner.sh
+++ data-preparation/update-scripts/_downloadBlazegraphRunner.sh
@@ -1,7 +1,9 @@
 export BLAZEGRAPH_RUNNER_RELEASE_URL=https://github.com/balhoff/blazegraph-runner/releases/download/v1.7/blazegraph-runner-1.7.tgz
-
+set -e
 # Download and extract the blazegraph-runner
 wget -O blazegraph-runner.tgz ${BLAZEGRAPH_RUNNER_RELEASE_URL}
+# Make ../utils directory if it does not exist
+mkdir -p ../utils
 # Extract the blazegraph-runner in the ../utils directory
 tar -xzf blazegraph-runner.tgz -C ../utils
 # Remove the version number from the directory name
