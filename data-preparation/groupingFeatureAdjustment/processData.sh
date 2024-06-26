ad56fb72e988319dd355bcd92944d830e6b2f0f8
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-06-26 11:24:15 +0200

    remove previous scripts

--- data-preparation/groupingFeatureAdjustment/processData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p ./data
-mkdir -p ./output
-
-# the container image 'process_same_as_statements' needs to be built using ./buildImage.sh
-docker run --rm -it -v ${PWD}/data:/usr/src/app/data -v ${PWD}/output:/usr/src/app/output process_same_as_statements
