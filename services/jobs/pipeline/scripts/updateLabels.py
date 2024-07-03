beb9880e29e0a90d02d56f61c3c9d832d65168b0
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-07-03 12:40:43 +0200

    read predicates from respective JSON

--- services/jobs/pipeline/scripts/updateLabels.py
+++ services/jobs/pipeline/scripts/updateLabels.py
@@ -6,46 +6,37 @@ def main(predicate_file, blazegraph_journal):
     with open('/pipeline/tmp/responses/label_count_by_graph.json', 'r') as f:
         count_json = json.load(f)
     with open(predicate_file, 'r') as f:
-        predicates = f.read()
+        predicates = json.load(f)
         
-    graph_create_query = 'CREATE GRAPH <http://schema.swissartresearch.net/rds/labels>'
-    with open('/pipeline/tmp/requests/graph_create_query.rq', 'w') as f:
-        f.write(graph_create_query)    
-    bash_command = '../utils/blazegraph-runner/bin/blazegraph-runner update --journal={0} /pipeline/tmp/requests/graph_create_query.rq'.format(blazegraph_journal)
-    try:
-        os.system(bash_command)
-    except:
-        pass
-
     file_num = 0
     graph2nb = {count_json['results']['bindings'][i]['graph_name']['value'] : int(count_json['results']['bindings'][i]['count']['value']) for i in range(len(count_json['results']['bindings']))}
     #print(graph2nb) 
     for graph, nb in graph2nb.items():
-        counter = 0
-        while counter <= nb:
-            query = """
-            INSERT {{ GRAPH <http://schema.swissartresearch.net/rds/labels> {{
-            ?subject <http://schema.swissartresearch.net/ontology/rds#label> ?label . 
-            }} 
-            }} WHERE {{
-                {{
-                    SELECT * {{
-                        GRAPH <{0}> {{
-                            ?subject {1} ?label .
-                                    }}
-                            }} ORDER BY DESC(?subject) OFFSET {2} LIMIT 3000000
-                 }}
-            }}
-            """.format(graph, predicates, str(counter))            
-            counter = counter + 3000000
-            print(graph)
-            with open('/pipeline/tmp/requests/label_query.rq', 'w') as f:
-                f.write(query)
-            print(query)
-            bash_command = '../utils/blazegraph-runner/bin/blazegraph-runner update --journal={0} /pipeline/tmp/requests/label_query.rq'.format(blazegraph_journal, file_num)
-            os.system(bash_command)
+        if graph in predicates and len(predicates[graph]):
+            counter = 0
+            predicatesPath = '|'.join(predicates[graph])
+            while counter <= nb:
+                query = """
+                INSERT {{ GRAPH <http://schema.swissartresearch.net/rds/labels> {{
+                ?subject <http://schema.swissartresearch.net/ontology/rds#label> ?label . 
+                }} 
+                }} WHERE {{
+                    {{
+                        SELECT * {{
+                            GRAPH <{0}> {{
+                                ?subject {1} ?label .
+                                        }}
+                                }} ORDER BY DESC(?subject) OFFSET {2} LIMIT 3000000
+                    }}
+                }}
+                """.format(graph, predicatesPath, str(counter))            
+                counter = counter + 3000000
+                with open('/pipeline/tmp/requests/label_query.rq', 'w') as f:
+                    f.write(query)
+                bash_command = '../utils/blazegraph-runner/bin/blazegraph-runner update --journal={0} /pipeline/tmp/requests/label_query.rq'.format(blazegraph_journal, file_num)
+                os.system(bash_command)
 
-            file_num = file_num + 1
+                file_num = file_num + 1
             
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
