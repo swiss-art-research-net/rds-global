448505618f342d9af5b310a683f0803db3946d3c
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-07-03 15:24:05 +0200

    chore: Update updateLabels.py to limit the update to a specific graph

--- services/jobs/pipeline/scripts/updateLabels.py
+++ services/jobs/pipeline/scripts/updateLabels.py
@@ -2,11 +2,17 @@ import os
 import argparse
 import json
 
-def main(predicate_file, blazegraph_journal):
+def main(predicate_file, blazegraph_journal, limit_graph=None):
     with open('/pipeline/tmp/responses/label_count_by_graph.json', 'r') as f:
         count_json = json.load(f)
     with open(predicate_file, 'r') as f:
         predicates = json.load(f)
+
+    # if limit_graph is set, remove all keys from predicate apart from the specified graph
+    if limit_graph:
+        for graph in predicates.keys():
+            if graph != limit_graph:
+                predicates[graph] = []
         
     file_num = 0
     graph2nb = {count_json['results']['bindings'][i]['graph_name']['value'] : int(count_json['results']['bindings'][i]['count']['value']) for i in range(len(count_json['results']['bindings']))}
@@ -44,9 +50,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Produce ttl files with entities and their labels using a unified RDS predicate <http://schema.swissartresearch.net/ontology/rds#label>')
     parser.add_argument('--predicate_file', required=True,help='file with predicate to use to query for entity labels')
     parser.add_argument('--blazegraph_journal',required=True, help='path to blazegraph journal file')
+    parser.add_argument('--limit_graph', required=False, help='limit the update to a specific graph')
     
     args = parser.parse_args()
     predicate_file = args.predicate_file
     blazegraph_journal = args.blazegraph_journal
+    if args.limit_graph:
+        limit_graph = args.limit_graph
+    else:
+        limit_graph = None
     
-    main(predicate_file, blazegraph_journal)
+    main(predicate_file, blazegraph_journal, limit_graph)
