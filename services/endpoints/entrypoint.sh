e53389e157ab904aa92012b125af44922365fd0e
Florian Kräutli <florian.kraeutli@uzh.ch>
2026-01-07 10:23:12 +0100

    refactor: improve readability of location blocks in entrypoint.sh

--- services/endpoints/entrypoint.sh
+++ services/endpoints/entrypoint.sh
@@ -17,9 +17,18 @@ mkdir -p "$OUT_DIR"
 
     cat <<EOF
 
-location = /${name} { return 301 /${name}/?\$args; }
+location = /${name} {
+  if (\$request_method = 'OPTIONS') {
+    return 204;
+  }
+  return 301 /${name}/?\$args;
+}
 
 location ^~ /${name}/ {
+  if (\$request_method = 'OPTIONS') {
+    return 204;
+  }
+
   proxy_set_header Host \$host;
   proxy_set_header X-Real-IP \$remote_addr;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
