09ee6d85d6c3c22d4eeb5b158c0bd6108c4383a2
Florian Kräutli <florian.kraeutli@uzh.ch>
2026-01-07 10:29:11 +0100

    refactor: clean up whitespace and improve rewrite rule for WebSocket support in entrypoint.sh

--- services/endpoints/entrypoint.sh
+++ services/endpoints/entrypoint.sh
@@ -29,16 +29,19 @@ location ^~ /${name}/ {
   proxy_http_version 1.1;
   proxy_set_header Upgrade \$http_upgrade;
   proxy_set_header Connection "upgrade";
-
+  
   proxy_set_header Host \$host;
   proxy_set_header X-Real-IP \$remote_addr;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   proxy_set_header X-Forwarded-Proto \$scheme;
 
+  rewrite ^/${name}/(.*)$ /\$1 break;
   rewrite ^/${name}/$ / break;
-  rewrite ^/${name}/(.*)$ /$1 break;
-  
+
   proxy_pass http://qlever:${port};
+  
+  # Ensure the proxy buffer is off for WebSockets/Streaming
+  proxy_buffering off;
 }
 EOF
   done
