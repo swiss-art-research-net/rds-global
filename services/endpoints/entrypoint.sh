da0f4ec6d730d50d6cc1c1222e8aa7edec222fcd
Florian Kräutli <florian.kraeutli@uzh.ch>
2026-01-07 15:45:00 +0100

    refactor: update Content-Type handling in entrypoint.sh and default.conf

--- services/endpoints/entrypoint.sh
+++ services/endpoints/entrypoint.sh
@@ -28,34 +28,26 @@ location = /${name} {
 }
 
 location ^~ /${name}/ {
-  if (\$request_method = 'OPTIONS') {
-    add_header Access-Control-Allow-Origin "*" always;
-    add_header Access-Control-Allow-Methods "GET, POST, OPTIONS" always;
-    add_header Access-Control-Allow-Headers "*" always;
-    return 204;
-  }
+    if (\$request_method = 'OPTIONS') {
+        add_header Access-Control-Allow-Origin "*" always;
+        add_header Access-Control-Allow-Methods "GET, POST, OPTIONS" always;
+        add_header Access-Control-Allow-Headers "*" always;
+        return 204;
+    }
 
-  proxy_http_version 1.1;
-  proxy_set_header Upgrade \$http_upgrade;
-  proxy_set_header Connection "upgrade";
-  
-  proxy_set_header Host \$host;
-  proxy_set_header X-Real-IP \$remote_addr;
-  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_http_version 1.1;
+    
+    proxy_set_body \$args;
+    
+    proxy_set_header Content-Type "application/x-www-form-urlencoded";
 
-  proxy_set_header Content-Type \$passed_content_type;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
 
-  proxy_pass_request_body on;
-  proxy_pass_request_headers on;
+    proxy_pass http://qlever:${port}/?;
 
-  client_max_body_size 100M;
-  proxy_request_buffering off;
-  
-  proxy_pass http://qlever:${port}/;
-  
-  proxy_buffering off;
-  proxy_read_timeout 300s;
+    proxy_buffering off;
+    proxy_read_timeout 300s;
 }
 EOF
   done
