d926fea644168bf7d815e5290d40d9a8d77cbb9b
jbatin <josip.batin@gmail.com>
2026-02-25 13:30:08 +0100

    added utils fns

---
+++ services/jobs/pipeline/scripts/lib/utils.py
@@ -0,0 +1,13 @@
+import yaml
+
+RDS_GRAPH_NAMESPACE = "http://schema.swissartresearch.net/rds/graph/"
+RDS_ONTOLOGY_NAMESPACE = "http://schema.swissartresearch.net/ontology/rds#"
+
+def generate_prefixes_for_SPARQL(prefixes):
+    items = sorted(prefixes.items(), key=lambda kv: kv[0])
+    return "\n".join([f"PREFIX {p}: <{uri}>" for p, uri in items])
+
+def load_config(path):
+    with open(path, "r", encoding="utf-8") as f:
+        return yaml.safe_load(f)
+    
